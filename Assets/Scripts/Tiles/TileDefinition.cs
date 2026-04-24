0ec4467ae09783d3f859d44cb2d311d7c7193d56
Liam Tolbert <lct4am@virginia.edu>
2026-04-24 03:58:04 -0400

    Big level generation rewrite, still needs tweaking

--- Assets/Scripts/Tiles/TileDefinition.cs
+++ Assets/Scripts/Tiles/TileDefinition.cs
@@ -17,12 +17,25 @@ namespace Tiles
             Closed
         }
 
-        public EdgeType north;
-        public EdgeType east;
-        public EdgeType south;
-        public EdgeType west;
+        // Direction index mapping (used by OrientedTile.GetEdge and WFCManager.GetDirIndex):
+        //   0 = posZ  (Vector2Int.up    / world +Z / forward)
+        //   1 = posX  (Vector2Int.right / world +X / right)
+        //   2 = negZ  (Vector2Int.down  / world -Z / back)
+        //   3 = negX  (Vector2Int.left  / world -X / left)
+        public EdgeType posZ;
+        public EdgeType posX;
+        public EdgeType negZ;
+        public EdgeType negX;
 
+        public enum TileCategory
+        {
+            Open,
+            Building,
+            Transition
+        }
 
+        [Tooltip("Cluster group. WFC biases tiles of the same category to group together.")]
+        public TileCategory category = TileCategory.Open;
     }
 
 }
