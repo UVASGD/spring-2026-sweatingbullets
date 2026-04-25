bbafac680ba7e1ed26abab9787546cec93f26ff6
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-04-24 23:20:02 -0400

    main-menu fade

---
+++ Assets/Scripts/MainMenu/SceneLoader.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void mainStart()
+    {
+        Debug.Log("Button clicked - starting countdown");
+        // Start the coroutine that handles the waiting AND the loading
+        StartCoroutine(WaitAndLoad(5f));
+    }
+
+    // This is now one sequence: Wait -> Then Load
+    public IEnumerator WaitAndLoad(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        Debug.Log("Time up! Loading scene...");
+        SceneManager.LoadScene("Main");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game has been quitted");
+    }
+}
